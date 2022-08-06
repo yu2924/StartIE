fff4a409a913f71a4925460fa1087a3e3793c049
yu2924 <88780921+yu2924@users.noreply.github.com>
2022-08-07 01:00:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# StartIE
+
+ ## what is this?
+
+launches the classic Internet Explorer using the classic Win32 API on modern Windows.  
+may work on Windows XP or later (unconfirmed)
+
+## requirements
+* Windows SDK
+* ATL
+
+## usage
+just run the command or double click the icon.  
+no parameters required.
+```
+> StartIE
+```
+
+## created by
+[yu2924](https://twitter.com/yu2924)
+
+## license
+CC0 1.0 Universal
---
+++ StartIE.cpp
@@ -0,0 +1,48 @@
+//
+//  StartIE.cpp
+//  StartIE
+//
+//  created by yu2924 on 2022-08-07
+//  (c) 2022 yu2924
+//
+
+#include "targetver.h"
+#include <atlbase.h>
+#include <atlstr.h>
+#include <strsafe.h>
+#include "resource.h"
+
+static CString GetErrorText(HRESULT r)
+{
+    TCHAR s[256];
+    if(!FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, r, 0, s, _countof(s), NULL))
+        if(!FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, HRESULT_CODE(r), 0, s, _countof(s), NULL))
+            StringCchPrintfW(s, _countof(s), L"0x%08X", r);
+    StrTrim(s, _T(" \r\n"));
+    return s;
+}
+
+struct ScopedCoinitialize
+{
+    ScopedCoinitialize(DWORD coinit) { CoInitializeEx(NULL, coinit); }
+    ~ScopedCoinitialize() { CoUninitialize(); }
+};
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+    ScopedCoinitialize coinit(COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+    try
+    {
+        HRESULT r = S_OK;
+        CComPtr<IDispatch> disp;
+        if(FAILED(r = disp.CoCreateInstance(L"InternetExplorer.Application", NULL, CLSCTX_LOCAL_SERVER))) throw r;
+        CComVariant visible(true);
+        if(FAILED(r = disp.PutPropertyByName(L"Visible", &visible))) throw r;
+        r = S_OK;
+    }
+    catch(HRESULT r)
+    {
+        MessageBoxW(NULL, CString(L"Error\n") + GetErrorText(r), CString(MAKEINTRESOURCE(IDR_STARTIE)), MB_ICONERROR | MB_OK);
+    }
+    return 0;
+}
